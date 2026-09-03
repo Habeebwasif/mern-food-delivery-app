165b301575a77e3643e69f31205393499eaefd06
HabeebWasif <habeeeb0910@gmail.com>
2026-09-03 17:23:42 +0000

    Configure application for EC2 deployment

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "http://54.219.152.240:4000";;
   const [token, setToken] = useState("");
 
   //remove food_list state 
